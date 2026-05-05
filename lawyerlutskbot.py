bc348919afe346dd143f12dd8a65537ac7f553ae
holly0254324-bit <0254324@gmail.com>
2026-05-05 12:49:51 +0300

    Update lawyerlutskbot.py

--- lawyerlutskbot.py
+++ lawyerlutskbot.py
@@ -53,12 +53,11 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     ]
 
    await update.message.reply_text(
-    "✨ Вас вітає приватний консультант Холлі!\n"
-    "Тут ви можете записатись до мене на прийом:\n"
-    "✨ Welcome to Holly, a private consultant!\n"
-    "You can make an appointment with me here:",
-    reply_markup=InlineKeyboardMarkup(keyboard)
-)
+       "✨ Вас вітає приватний консультант Холлі! Тут ви можете записатись до мене на прийом:\n"
+       "✨ Welcome to Holly, a private consultant!\n"
+       "You can make an appointment with me here:",
+       reply_markup=InlineKeyboardMarkup(keyboard)
+    )
 
 
 # ---------- TYPE SELECT ----------
